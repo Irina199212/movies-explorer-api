da5138b28801eb09720d3bc92a199bd2438094e4
Irina <irinawork1992@yandex.ru>
2023-04-19 23:10:51 +0300

    fix get movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,7 +5,7 @@ const AccessError = require('../errors/access');
 const ValidationError = require('../errors/validation');
 
 module.exports.getMovies = (req, res, next) => {
-  Movie.find({})
+  Movie.find({ owner: req.user._id })
     .then((movies) => res.send({ data: movies }))
     .catch((err) => {
       next(err);
